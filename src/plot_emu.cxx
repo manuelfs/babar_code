34b2a82e69cbfc84b63aeaa024868bfed66af78e
Manuel Franco Sevilla <manuel.franco.sevilla@cern.ch>
2020-09-20 09:39:42 -0400

    Updates RDx to 2019, added e/mu comparison

--- src/plot_emu.cxx
+++ src/plot_emu.cxx
@@ -39,7 +39,7 @@ int main(){
 
   // Numbers from HFLAV Spring 2019
   // https://hflav-eos.web.cern.ch/hflav-eos/semi/spring19/html/RDsDsstar/RDRDs.html
-  Results RD_SM ("SM pred.", 0.299, {0.003}, {0}, kBlue-4);
+  Results RD_SM ("SM pred.", 0.299, {0.003}, {0}, kOrange);
   Results RDs_SM("SM pred.", 0.258, {0.005}, {0}, kViolet+2);
 
   Results RD_HFLAV ("HFLAV aver.", 0.340, {0.027}, {0.013}, kRed, -0.38);
@@ -48,37 +48,38 @@ int main(){
   Results RD_BABAR ("BABAR (HT)", 0.440, {0.058}, {0.042}, kGreen+2, -0.31);
   Results RDs_BABAR("BABAR (HT)", 0.332, {0.024}, {0.018}, kGreen+2, -0.31);
 
-  Results RD_BelleHT ("Belle (HT)", 0.375, {0.064}, {0.026}, kOrange-7, -0.50);
-  Results RDs_BelleHT("Belle (HT)", 0.293, {0.038}, {0.015}, kBlue, -0.50);
+  Results RD_BABARe ("BaBaR HT electron", 0.3466, {0.0823}, {0.04}, kGreen+1, -0.41);
+  Results RDs_BABARe("BaBaR HT electron", 0.3671, {0.0342}, {0.02}, kGreen+1, -0.41);
 
-  Results RD_BelleST("Belle (ST)", 0.307, {0.037}, {0.016}, kOrange, -0.51);
-  Results RDs_BelleST("Belle (ST)", 0.283, {0.018}, {0.014}, kOrange, -0.51);
+  Results RD_BABARmu ("BaBaR HT muon", 0.5215, {0.0796}, {0.04}, kGreen-3, -0.41);
+  Results RDs_BABARmu("BaBaR HT muon", 0.2899, {0.0326}, {0.02}, kGreen-3, -0.41);
 
-  Results RDs_Bellepi("Belle (#pi/#rho)", 0.270, {0.035}, {0.027}, kOrange+1);
+  Results RD_BelleST("Belle (ST)", 0.307, {0.037}, {0.016}, kBlue+3, -0.52);
+  Results RDs_BelleST("Belle (ST)", 0.283, {0.018}, {0.014}, kBlue+3, -0.52);
 
-  Results RDs_LHCb("LHCb (#mu)", 0.336, {0.027}, {0.030}, kCyan-2);
-  Results RDs_LHCb2("LHCb (#pi#pi#pi)", 0.280, {0.018}, {0.029}, kMagenta+2);
+  Results RD_BelleSTe("Belle ST electron", 0.281, {0.042}, {0.017}, kAzure+1, -0.52);
+  Results RDs_BelleSTe("Belle ST electron", 0.304, {0.022}, {0.016}, kAzure+1, -0.52);
+
+  Results RD_BelleSTmu("Belle ST muon", 0.373, {0.068}, {0.030}, kBlue-4, -0.52);
+  Results RDs_BelleSTmu("Belle ST muon", 0.245, {0.035}, {0.020}, kBlue-4, -0.52);
+
 
   if(isWide){
-    RD_BABAR.name = "BaBar, PRL #font[62]{109}, 101802 (2012)";
-    RD_BelleHT.name = "Belle, PRD #font[62]{92}, 072014 (2015)";
+    RD_BABAR.name = "BaBar HT, PRL #font[62]{109}, 101802 (2012)";
     //RDs_BelleST.name = "Belle, PRD #font[62]{94}, 072007 (2016)";
-    RD_BelleST.name = "Belle, Moriond EW (2019)";
-    RDs_BelleST.name = "Belle, Moriond EW (2019)";
-    RDs_Bellepi.name = "Belle, PRL #font[62]{118}, 211801 (2017)";
-    RDs_LHCb.name = "LHCb, PRL #font[62]{115}, 111803 (2015)";
-    RDs_LHCb2.name = "LHCb, PRL #font[62]{120}, 171802 (2018)";
+    RD_BelleST.name = "Belle ST, PRL #font[62]{124}, 161803 (2020)";
+    RDs_BelleST.name = "Belle, PRL #font[62]{124}, 161803 (2020)";
     RD_HFLAV.name = "HFLAV average Spring 2019";
     RD_SM.name = "SM predictions";
   }
 
   vector<vector<Results> > results;
   results.push_back({RD_BABAR,  RDs_BABAR});
-  results.push_back({RDs_LHCb});
-  results.push_back({RD_BelleHT,  RDs_BelleHT});
-  results.push_back({RDs_Bellepi});
-  results.push_back({RDs_LHCb2});
   results.push_back({RD_BelleST, RDs_BelleST});
+  results.push_back({RD_BABARe,  RDs_BABARe});
+  results.push_back({RD_BelleSTe, RDs_BelleSTe});
+  results.push_back({RD_BABARmu,  RDs_BABARmu});
+  results.push_back({RD_BelleSTmu, RDs_BelleSTmu});
   results.push_back({RD_HFLAV,  RDs_HFLAV});
   results.push_back({RD_SM,    RDs_SM});
 
@@ -111,12 +112,12 @@ int main(){
 
   //////// Creating canvas and base histogram
   int cW = 500, cH = 380;
-  if(isWide) cW = 800;
+  if(isWide) cW = 880;
   TCanvas can("can","", cW, cH);
   //TCanvas can("can","", 500, 410);
   float minX=0.20, maxX=0.54, minY=0.21, maxY=0.49;
   if(isWide){
-    minX = 0.19; maxX = 0.54; minY = 0.207; maxY = 0.52;
+    minX = 0.21; maxX = 0.63; minY = 0.17; maxY = 0.62;
   }
   TH1D histo("histo", "", 10, minX, maxX);
   histo.SetMinimum(minY);
@@ -146,6 +147,9 @@ int main(){
 	ellipse.SetFillColorAlpha(result[0].color, alpha+0.32);
       else ellipse.SetFillColorAlpha(result[0].color, alpha);
       ellipse.SetLineColor(result[0].color); ellipse.SetLineWidth(lWidth);
+      if(result[0].name.Contains("electron")) ellipse.SetLineStyle(2);
+      else if(result[0].name.Contains("muon")) ellipse.SetLineStyle(3);
+      else ellipse.SetLineStyle(1);
       ellipse.DrawEllipse(rd, rds, maxR, minR, 0, 360, angle, "c");
       if(result[0].name.Contains("SM")) ellipse.DrawEllipse(rd, rds, maxR, minR, 0, 360*10, angle, "c");
       //cout<<"RDs = "<<rds<<" +- "<<erds/rds*100<<endl;
@@ -181,18 +185,18 @@ int main(){
     Results RDs_HFLAV4("noplot", RDs_HFLAV.value, {static_cast<float>(3.4* RDs_HFLAV.errUp())}, {0}, kRed, RD_HFLAV.correl);
     vector<Results>  result({RD_HFLAV4,  RDs_HFLAV4});
 
-    float rd = result[0].value, rds = result[1].value;
-    float erd = result[0].errUp(), erds = result[1].errUp();
-    getEllipse(erd, erds, result[0].correl, maxR, minR, angle);
-    ellipse.SetFillStyle(0);
-    ellipse.SetLineWidth(1);
-    ellipse.SetLineStyle(2);
-    ellipse.SetLineColor(result[0].color); 
-    ellipse.DrawEllipse(rd, rds, maxR, minR, 0, 360, angle, "c");
-    TLatex label;  //label.SetNDC(kTRUE);
-    label.SetTextAlign(23); label.SetTextSize(0.06);
-    label.SetTextColor(kRed+1);
-    label.DrawLatex(0.3, 0.365, "#font[62]{3#sigma}");
+    // float rd = result[0].value, rds = result[1].value;
+    // float erd = result[0].errUp(), erds = result[1].errUp();
+    // getEllipse(erd, erds, result[0].correl, maxR, minR, angle);
+    // ellipse.SetFillStyle(0);
+    // ellipse.SetLineWidth(1);
+    // ellipse.SetLineStyle(2);
+    // ellipse.SetLineColor(result[0].color); 
+    // ellipse.DrawEllipse(rd, rds, maxR, minR, 0, 360, angle, "c");
+    // TLatex label;  //label.SetNDC(kTRUE);
+    // label.SetTextAlign(23); label.SetTextSize(0.06);
+    // label.SetTextColor(kRed+1);
+    // label.DrawLatex(0.3, 0.365, "#font[62]{3#sigma}");
 
   }
 
@@ -206,9 +210,9 @@ int main(){
   } else if(results.size()<=6) legH = legSingle*(8+Ncols-1)/Ncols/3*2;
   if(isWide) {
     Ncols = 1;
-    legX -= 0.01;
+    legX -= 0.02;
     leg2X = legX+0.37;
-    legW = 0.1;
+    legW = 0.15;
     legH = legSingle * results.size()/1.8;
     legFontSize = 0.055;
   } 
@@ -235,6 +239,9 @@ int main(){
       hLeg.back()->SetLineWidth(lWidth);
       hLeg.back()->SetLineColor(results[ind][0].color);
       hLeg.back()->SetFillColorAlpha(results[ind][0].color, alpha);
+      if(results[ind][0].name.Contains("electron")) hLeg.back()->SetLineStyle(2);
+      else if(results[ind][0].name.Contains("muon")) hLeg.back()->SetLineStyle(3);
+      else hLeg.back()->SetLineStyle(1);
       if(ind%2==0) leg.AddEntry((hLeg.back()), results[ind][0].name, "lf");
       else leg2.AddEntry((hLeg.back()), results[ind][0].name, "lf");
     }
@@ -244,7 +251,7 @@ int main(){
   histo.Draw("same axis");
 
 
-  TString plotName = "plots/rdx_ellipses.pdf";
+  TString plotName = "plots/rdx_emu.pdf";
   can.SaveAs(plotName);
 
   return 0;
